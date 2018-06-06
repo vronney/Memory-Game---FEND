eb67a55ef4f54e8c3b6411ba3af33d2b10a9a16e
Ronney Vargas <37166515+vronney@users.noreply.github.com>
2018-06-06 18:20:58 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,170 +1,165 @@
 let symbols = ['diamond', 'diamond', 'paper-plane-o', 'paper-plane-o', 'anchor', 'anchor', 'bolt', 'bolt', 'cube', 'cube', 'leaf', 'leaf', 'bicycle', 'bicycle', 'bomb', 'bomb'],
-    opened = [],
-    match = 0,
-    Clicks = 0,
-    $Playground = $('.Playground'),
-    $scorePanel = $('#score-panel'),
-    $moveNum = $('.Clicks'),
-    $ratingStars = $('.fa-thumb-up'),
-    $PlayAgain = $('.PlayAgain'),
-    delay = 400,
-    currentseconds,
-    second = 0,
-    $seconds = $('.seconds'),
-    totalbox = symbols.length / 2,
-    rank3stars = 10,
-    rank2stars = 16,
-    rank1stars = 20;
+  opened = [],
+	match = 0,
+	Clicks = 0,
+	$Playground = $('.Playground'),
+	$scorePanel = $('#score-panel'),
+	$moveNum = $('.Clicks'),
+	$ratingStars = $('.fa-angellist'),
+	$PlayAgain = $('.PlayAgain'),
+	delay = 400,
+	currentseconds,
+	second = 0,
+	$seconds = $('.seconds'),
+	totalbox = symbols.length / 2,
+	rank3stars = 10,
+	rank2stars = 15,
+	rank1stars = 20;
 
 function shuffle(array) {
-    var currentIndex = array.length,
-        temporaryValue, randomIndex;
+	var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (0 !== currentIndex) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+	while (0 !== currentIndex) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-    return array;
+	return array;
 }
 
 // Initial Game
 function initGame() {
-    var boxes = shuffle(symbols);
-    $Playground.empty();
-    match = 0;
-    Clicks = 0;
-    $moveNum.text('0');
-    $ratingStars.removeClass('fa-thumbs-down').addClass('fa-star');
-    for (var i = 0; i < boxes.length; i++) {
-        $Playground.append($('<li class="box"><i class="fa fa-' + boxes[i] + '"></i></li>'))
-    }
-    addboxListener();
+	var boxes = shuffle(symbols);
+	$Playground.empty();
+	match = 0;
+	Clicks = 0;
+	$moveNum.text('0');
+	$ratingStars.removeClass('fa-thumbs-down').addClass('fa-angellist');
+	for (var i = 0; i < boxes.length; i++) {
+		$Playground.append($('<li class="box"><i class="fa fa-' + boxes[i] + '"></i></li>'))
+	}
+	addboxListener();
 
-    resetseconds(currentseconds);
-    second = 0;
-    $seconds.text(`${second}`)
-    initTime();
+	resetseconds(currentseconds);
+	second = 0;
+	$seconds.text(`${second}`)
+	initTime();
 };
 
 // Set Rating and final Score
 function setRating(moves) {
-    var rating = 3;
-    if (moves > rank3stars && moves < rank2stars) {
-        $ratingStars.eq(2).removeClass('fa-thumbs-up').addClass('fa-thumbs-down');
-        rating = 2;
-    } else if (moves > rank2stars && moves < rank1stars) {
-        $ratingStars.eq(1).removeClass('fa-thumbs-up').addClass('fa-thumbs-down');
-        rating = 1;
-    } else if (moves > rank1stars) {
-        $ratingStars.eq(0).removeClass('fa-thumbs-up').addClass('fa-thumbs-down');
-        rating = 0;
-    }
-    return {
-        score: rating
-    };
+	var rating = 3;
+	if (moves > rank3stars && moves < rank2stars) {
+		$ratingStars.eq(2).removeClass('fa-angellist').addClass('fa-thumbs-down');
+		rating = 2;
+	} else if (moves > rank2stars && moves < rank1stars) {
+		$ratingStars.eq(1).removeClass('fa-angellist').addClass('fa-thumbs-down');
+		rating = 1;
+	} else if (moves > rank1stars) {
+		$ratingStars.eq(0).removeClass('fa-angellist').addClass('fa-thumbs-down');
+		rating = 0;
+	}
+	return { score: rating };
 };
 
 
 // End Game
 function endGame(Clicks, score) {
-    swal({
-        allowEscapeKey: false,
-        allowOutsideClick: false,
-        title: 'Congratulations! You Won!',
-        text: 'With ' + Clicks + ' Clicks and ' + score + ' Stars in ' + second + ' Seconds.\n Woooooo!',
-        type: 'success',
-        confirmButtonColor: '#02ccba',
-        confirmButtonText: 'Play again!'
-    }).then(function(isConfirm) {
-        if (isConfirm) {
-            initGame();
-        }
-    })
+	swal({
+		allowEscapeKey: false,
+		allowOutsideClick: false,
+		title: 'Congratulations! You Won!',
+		text: 'With ' + Clicks + ' Clicks and ' + score + ' Stars in ' + second + ' Seconds.\n Woooooo!',
+		type: 'success',
+		confirmButtonColor: '#02ccba',
+		confirmButtonText: 'Play again!'
+	}).then(function (isConfirm) {
+		if (isConfirm) {
+			initGame();
+		}
+	})
 }
 
 // PlayAgain Game
-$PlayAgain.bind('click', function() {
-    swal({
-        allowEscapeKey: false,
-        allowOutsideClick: false,
-        title: 'You need to be a surrender',
-        text: "Are you sure Mr. Noop",
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#02ccba',
-        cancelButtonColor: '#f95c3c',
-        confirmButtonText: 'Yes, Im noop',
-    }).then(function(isConfirm) {
-        if (isConfirm) {
-            initGame();
-        }
-    })
+$PlayAgain.bind('click', function () {
+	swal({
+		allowEscapeKey: false,
+		allowOutsideClick: false,
+		title: 'You need to be a surrender',
+		text: "Are you sure Mr. Noop",
+		type: 'warning',
+		showCancelButton: true,
+		confirmButtonColor: '#02ccba',
+		cancelButtonColor: '#f95c3c',
+		confirmButtonText: 'Yes, Im noop',
+	}).then(function (isConfirm) {
+		if (isConfirm) {
+			initGame();
+		}
+	})
 });
 
-var addboxListener = function() {
+var addboxListener = function () {
 
-    // box flip
-    $Playground.find('.box').bind('click', function() {
-        var $this = $(this)
+	// box flip
+	$Playground.find('.box').bind('click', function () {
+		var $this = $(this)
 
-        if ($this.hasClass('show') || $this.hasClass('match')) {
-            return true;
-        }
+		if ($this.hasClass('show') || $this.hasClass('match')) { return true; }
 
-        var box = $this.context.innerHTML;
-        $this.addClass('open show');
-        opened.push(box);
+		var box = $this.context.innerHTML;
+		$this.addClass('open show');
+		opened.push(box);
 
-        // Compare with opened box
-        if (opened.length > 1) {
-            if (box === opened[0]) {
-                $Playground.find('.open').addClass('match animated infinite rubberBand');
-                setTimeout(function() {
-                    $Playground.find('.match').removeClass('open show animated infinite rubberBand');
-                }, delay);
-                match++;
-            } else {
-                $Playground.find('.open').addClass('notmatch animated infinite wobble');
-                setTimeout(function() {
-                    $Playground.find('.open').removeClass('animated infinite wobble');
-                }, delay / 1.5);
-                setTimeout(function() {
-                    $Playground.find('.open').removeClass('open show notmatch animated infinite wobble');
-                }, delay);
-            }
-            opened = [];
-            Clicks++;
-            setRating(Clicks);
-            $moveNum.html(Clicks);
-        }
+		// Compare with opened box
+		if (opened.length > 1) {
+			if (box === opened[0]) {
+				$Playground.find('.open').addClass('match animated infinite rubberBand');
+				setTimeout(function () {
+					$Playground.find('.match').removeClass('open show animated infinite rubberBand');
+				}, delay);
+				match++;
+			} else {
+				$Playground.find('.open').addClass('notmatch animated infinite wobble');
+				setTimeout(function () {
+					$Playground.find('.open').removeClass('animated infinite wobble');
+				}, delay / 1.5);
+				setTimeout(function () {
+					$Playground.find('.open').removeClass('open show notmatch animated infinite wobble');
+				}, delay);
+			}
+			opened = [];
+			Clicks++;
+			setRating(Clicks);
+			$moveNum.html(Clicks);
+		}
 
-        // End Game if match all boxes
-        if (totalbox === match) {
-            setRating(Clicks);
-            var score = setRating(Clicks).score;
-            setTimeout(function() {
-                endGame(Clicks, score);
-            }, 500);
-        }
-    });
+		// End Game if match all boxes
+		if (totalbox === match) {
+			setRating(Clicks);
+			var score = setRating(Clicks).score;
+			setTimeout(function () {
+				endGame(Clicks, score);
+			}, 500);
+		}
+	});
 };
 
 
 function initTime() {
-    currentseconds = setInterval(function() {
-        $seconds.text(`${second}`)
-        second = second + 1
-    }, 1000);
+	currentseconds = setInterval(function () {
+		$seconds.text(`${second}`)
+		second = second + 1
+	}, 1000);
 }
 
 function resetseconds(seconds) {
-    if (seconds) {
-        clearInterval(seconds);
-    }
+	if (seconds) {
+		clearInterval(seconds);
+	}
 }
 
 initGame();
