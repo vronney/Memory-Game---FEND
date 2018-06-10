1e485e48326bf5dbabdb21ed877c9a01b0b374ed
Ronney Vargas <37166515+vronney@users.noreply.github.com>
2018-06-09 22:52:43 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -58,10 +58,8 @@ function setRating(moves) {
 	} else if (moves > rank2stars && moves < rank1stars) {
 		$ratingStars.eq(1).removeClass('fa-angellist').addClass('fa-thumbs-down');
 		rating = 1;
-	} else if (moves > rank1stars) {
-		$ratingStars.eq(0).removeClass('fa-angellist').addClass('fa-thumbs-down');
-		rating = 0;
-	}
+	} 
+	
 	return { score: rating };
 };
 
