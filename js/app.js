8f0cd7676cf2003cf4849c6808f0df52a3859ee7
Ronney Vargas <37166515+vronney@users.noreply.github.com>
2018-08-18 21:47:52 -0500

    Fixed clock - starts when first card is clicked.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
-let symbols = ['diamond', 'diamond', 'paper-plane-o', 'paper-plane-o', 'anchor', 'anchor', 'bolt', 'bolt', 'cube', 'cube', 'leaf', 'leaf', 'bicycle', 'bicycle', 'bomb', 'bomb'],
- 	opened = [],
+let symbols = [
+	'diamond',
+	'diamond',
+	'paper-plane-o',
+	'paper-plane-o',
+	'anchor',
+	'anchor',
+	'bolt',
+	'bolt',
+	'cube',
+	'cube',
+	'leaf',
+	'leaf',
+	'bicycle',
+	'bicycle',
+	'bomb',
+	'bomb',
+],
+	opened = [],
 	match = 0,
 	Clicks = 0,
 	$Playground = $('.Playground'),
@@ -17,7 +34,9 @@ let symbols = ['diamond', 'diamond', 'paper-plane-o', 'paper-plane-o', 'anchor',
 	rank1stars = 20;
 
 function shuffle(array) {
-	var currentIndex = array.length, temporaryValue, randomIndex;
+	var currentIndex = array.length,
+		temporaryValue,
+		randomIndex;
 
 	while (0 !== currentIndex) {
 		randomIndex = Math.floor(Math.random() * currentIndex);
@@ -32,7 +51,7 @@ function shuffle(array) {
 
 // Initial Game
 function initGame() {
-  opened = [];
+	opened = [];
 	var boxes = shuffle(symbols);
 	$Playground.empty();
 	match = 0;
@@ -40,17 +59,18 @@ function initGame() {
 	$moveNum.text('0');
 	$ratingStars.removeClass('fa-thumbs-down').addClass('fa-angellist');
 	for (var i = 0; i < boxes.length; i++) {
-		$Playground.append($('<li class="box"><i class="fa fa-' + boxes[i] + '"></i></li>'))
+		$Playground.append(
+			$('<li class="box"><i class="fa fa-' + boxes[i] + '"></i></li>')
+		);
 	}
 	addboxListener();
 
-  resetseconds(currentseconds);
+	resetseconds(currentseconds);
 	second = 0;
 	$seconds.text(`${second}`);
 
-  timeClick = 0;
-
-};
+	timeClick = 0;
+}
 
 // Set Rating and final Score
 function setRating(moves) {
@@ -61,10 +81,9 @@ function setRating(moves) {
 	} else if (moves > rank2stars && moves < rank1stars) {
 		$ratingStars.eq(1).removeClass('fa-angellist').addClass('fa-thumbs-down');
 		rating = 1;
-	} 
+	}
 	return { score: rating };
-};
-
+}
 
 // End Game
 function endGame(Clicks, score) {
@@ -72,19 +91,26 @@ function endGame(Clicks, score) {
 		allowEscapeKey: false,
 		allowOutsideClick: false,
 		title: 'Congratulations! You Won!',
-		text: 'With ' + Clicks + ' Clicks and ' + score + ' Stars in ' + second + ' Seconds.\n Woooooo!',
+		text:
+			'With ' +
+				Clicks +
+				' Clicks and ' +
+				score +
+				' Stars in ' +
+				second +
+				' Seconds.\n Woooooo!',
 		type: 'success',
 		confirmButtonColor: '#02ccba',
-		confirmButtonText: 'Play again!'
-	}).then(function (isConfirm) {
+		confirmButtonText: 'Play again!',
+	}).then(function(isConfirm) {
 		if (isConfirm) {
 			initGame();
 		}
-	})
+	});
 }
 
 // PlayAgain Game
-$PlayAgain.bind('click', function () {
+$PlayAgain.bind('click', function() {
 	swal({
 		allowEscapeKey: false,
 		allowOutsideClick: false,
@@ -95,25 +121,25 @@ $PlayAgain.bind('click', function () {
 		confirmButtonColor: '#02ccba',
 		cancelButtonColor: '#f95c3c',
 		confirmButtonText: 'Yes, Im giving up.',
-	}).then(function (isConfirm) {
+	}).then(function(isConfirm) {
 		if (isConfirm) {
 			initGame();
 		}
-	})
+	});
 });
 
-var addboxListener = function () {
-
+var addboxListener = function() {
 	// box flip
-	$Playground.find('.box').bind('click', function () {
-		
-    timeClick = timeClick+1;
-    console.log(timeClick);
-    if (timeClick == 1) {
-      initTime();
-    };
-    var $this = $(this)
-		if ($this.hasClass('show') || $this.hasClass('match')) { return true; }
+	$Playground.find('.box').bind('click', function() {
+		timeClick = timeClick + 1;
+
+		if (timeClick == 1) {
+			initTime();
+		}
+		var $this = $(this);
+		if ($this.hasClass('show') || $this.hasClass('match')) {
+			return true;
+		}
 
 		var box = $this.context.innerHTML;
 		$this.addClass('open show');
@@ -122,18 +148,24 @@ var addboxListener = function () {
 		// Compare with opened box
 		if (opened.length > 1) {
 			if (box === opened[0]) {
-				$Playground.find('.open').addClass('match animated infinite rubberBand');
-				setTimeout(function () {
-					$Playground.find('.match').removeClass('open show animated infinite rubberBand');
+				$Playground
+					.find('.open')
+					.addClass('match animated infinite rubberBand');
+				setTimeout(function() {
+					$Playground
+						.find('.match')
+						.removeClass('open show animated infinite rubberBand');
 				}, delay);
 				match++;
 			} else {
 				$Playground.find('.open').addClass('notmatch animated infinite wobble');
-				setTimeout(function () {
+				setTimeout(function() {
 					$Playground.find('.open').removeClass('animated infinite wobble');
 				}, delay / 1.5);
-				setTimeout(function () {
-					$Playground.find('.open').removeClass('open show notmatch animated infinite wobble');
+				setTimeout(function() {
+					$Playground
+						.find('.open')
+						.removeClass('open show notmatch animated infinite wobble');
 				}, delay);
 			}
 			opened = [];
@@ -146,7 +178,7 @@ var addboxListener = function () {
 		if (totalbox === match) {
 			setRating(Clicks);
 			var score = setRating(Clicks).score;
-			setTimeout(function () {
+			setTimeout(function() {
 				endGame(Clicks, score);
 			}, 500);
 		}
@@ -154,10 +186,10 @@ var addboxListener = function () {
 };
 
 function initTime() {
-	currentseconds = setInterval(function () {
+	currentseconds = setInterval(function() {
 		$seconds.text(`${second}`);
 		second = second + 1;
-	    },1000);
+	}, 1000);
 }
 
 function resetseconds(seconds) {
@@ -165,6 +197,5 @@ function resetseconds(seconds) {
 		clearInterval(seconds);
 	}
 }
-
 
 initGame();
